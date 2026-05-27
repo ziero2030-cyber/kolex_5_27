5fa2fcfb7cd974732dea2564eafa8f8b570c3e5b
ziero2030-cyber <ziero2030@gmail.com>
2026-05-27 17:39:46 +0900

    데스크탑 진행

---
+++ javaScript/zen.js
@@ -0,0 +1,40 @@
+<script type="text/javascript" id="" charset="">(function(a,d,e,f,g,b,c){a.visitorGlobalObjectAlias=f;a[a.visitorGlobalObjectAlias]=a[a.visitorGlobalObjectAlias]||function(){(a[a.visitorGlobalObjectAlias].q=a[a.visitorGlobalObjectAlias].q||[]).push(arguments)};a[a.visitorGlobalObjectAlias].l=(new Date).getTime();b=d.createElement("script");b.src=e;b.async=!0;c=d.getElementsByTagName("script")[0];c.parentNode.insertBefore(b,c)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");vgo("setAccount","652360825");
+vgo("setTrackByDefault",!0);vgo("process");</script>
+
+<script type="text/javascript" id="" charset="">CE_FEATURES={ab_tests:{enabled:!1},editor:{enabled:!1}};location.hostname.substring(0,8)==="discover"&&(CE_FEATURES.ab_tests.enabled=!0,CE_FEATURES.editor.enabled=!0);</script>
+
+<script id="" text="" charset="" type="text/javascript" src="//script.crazyegg.com/pages/scripts/0126/6926.js"></script>
+
+<script id="" text="" charset="" type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
+
+<script type="text/javascript" id="" charset="">(function(a,b){var d="spdt-capture",e="script";if(!b.getElementById(d)){a.spdt=a.spdt||function(){(a.spdt.q=a.spdt.q||[]).push(arguments)};var c=b.createElement(e);c.id=d;c.async=1;c.src="https://pixel.byspotify.com/ping.min.js";b=b.getElementsByTagName(e)[0];b.parentNode.insertBefore(c,b)}a.spdt("conf",{key:"cc6c10a4dd9b42099ce4bad57275255a"});a.spdt("view")})(window,document);</script>
+
+<script type="text/javascript" id="" charset="">function loadScript(b){var c=document.getElementsByTagName("head")[0],a=document.createElement("script");a.type="text/javascript";a.src="https://tracker.metricool.com/resources/be.js";a.onreadystatechange=b;a.onload=b;c.appendChild(a)}loadScript(function(){beTracker.t({hash:"c14395587c264d9c1d42d8bb9cb8c00a"})});</script>
+
+<script type="text/javascript" id="" charset="">(function(){var f="utm_source",b=new URLSearchParams(window.location.search);b=b.get("utm_source");a:{var a=f+"\x3d";var c=decodeURIComponent(document.cookie);c=c.split(";");for(var d=0;d<c.length;d++){var e=c[d].trim();if(e.indexOf(a)===0){a=e.substring(a.length,e.length);break a}}a=null}console.log("utm_source from URL:",b);console.log("Current utm_source cookie value:",a);b&&b!==a?(console.log("Updating utm_source cookie..."),a=new Date,a.setTime(a.getTime()+6048E5),a="; expires\x3d"+a.toUTCString(),
+document.cookie=f+"\x3d"+b+a+"; path\x3d/; domain\x3d.zenmarket.jp",console.log("utm_source cookie updated:",b)):console.log("utm_source cookie not updated or no new value provided.")})();</script>
+
+<script type="text/javascript" id="" charset="">(function(){var f="utm_medium",b=new URLSearchParams(window.location.search);b=b.get("utm_medium");a:{var a=f+"\x3d";var c=decodeURIComponent(document.cookie);c=c.split(";");for(var d=0;d<c.length;d++){var e=c[d].trim();if(e.indexOf(a)===0){a=e.substring(a.length,e.length);break a}}a=null}console.log("utm_medium from URL:",b);console.log("Current utm_medium cookie value:",a);b&&b!==a?(console.log("Updating utm_medium cookie..."),a=new Date,a.setTime(a.getTime()+6048E5),a="; expires\x3d"+a.toUTCString(),
+document.cookie=f+"\x3d"+b+a+"; path\x3d/; domain\x3d.zenmarket.jp",console.log("utm_medium cookie updated:",b)):console.log("utm_medium cookie not updated or no new value provided.")})();</script>
+
+<script type="text/javascript" id="" charset="">(function(){var f="utm_campaign",b=new URLSearchParams(window.location.search);b=b.get("utm_campaign");a:{var a=f+"\x3d";var c=decodeURIComponent(document.cookie);c=c.split(";");for(var d=0;d<c.length;d++){var e=c[d].trim();if(e.indexOf(a)===0){a=e.substring(a.length,e.length);break a}}a=null}console.log("utm_campaign from URL:",b);console.log("Current utm_campaign cookie value:",a);b&&b!==a?(console.log("Updating utm_campaign cookie..."),a=new Date,a.setTime(a.getTime()+6048E5),a="; expires\x3d"+
+a.toUTCString(),document.cookie=f+"\x3d"+b+a+"; path\x3d/; domain\x3d.zenmarket.jp",console.log("utm_campaign cookie updated:",b)):console.log("utm_campaign cookie not updated or no new value provided.")})();</script>
+
+<script type="text/javascript" id="" charset="">(function(){var f="utm_id",b=new URLSearchParams(window.location.search);b=b.get("utm_id");a:{var a=f+"\x3d";var c=decodeURIComponent(document.cookie);c=c.split(";");for(var d=0;d<c.length;d++){var e=c[d].trim();if(e.indexOf(a)===0){a=e.substring(a.length,e.length);break a}}a=null}console.log("utm_id from URL:",b);console.log("Current utm_id cookie value:",a);b&&b!==a?(console.log("Updating utm_id cookie..."),a=new Date,a.setTime(a.getTime()+6048E5),a="; expires\x3d"+a.toUTCString(),document.cookie=
+f+"\x3d"+b+a+"; path\x3d/; domain\x3d.zenmarket.jp",console.log("utm_id cookie updated:",b)):console.log("utm_id cookie not updated or no new value provided.")})();</script>
+
+<script type="text/javascript" id="" charset="">(function(){var f="utm_term",b=new URLSearchParams(window.location.search);b=b.get("utm_term");a:{var a=f+"\x3d";var c=decodeURIComponent(document.cookie);c=c.split(";");for(var d=0;d<c.length;d++){var e=c[d].trim();if(e.indexOf(a)===0){a=e.substring(a.length,e.length);break a}}a=null}console.log("utm_term from URL:",b);console.log("Current utm_term cookie value:",a);b&&b!==a?(console.log("Updating utm_term cookie..."),a=new Date,a.setTime(a.getTime()+6048E5),a="; expires\x3d"+a.toUTCString(),document.cookie=
+f+"\x3d"+b+a+"; path\x3d/; domain\x3d.zenmarket.jp",console.log("utm_term cookie updated:",b)):console.log("utm_term cookie not updated or no new value provided.")})();</script>
+
+<script type="text/javascript" id="" charset="">(function(){var f="utm_content",b=new URLSearchParams(window.location.search);b=b.get("utm_content");a:{var a=f+"\x3d";var c=decodeURIComponent(document.cookie);c=c.split(";");for(var d=0;d<c.length;d++){var e=c[d].trim();if(e.indexOf(a)===0){a=e.substring(a.length,e.length);break a}}a=null}console.log("utm_content from URL:",b);console.log("Current utm_content cookie value:",a);b&&b!==a?(console.log("Updating utm_content cookie..."),a=new Date,a.setTime(a.getTime()+6048E5),a="; expires\x3d"+a.toUTCString(),
+document.cookie=f+"\x3d"+b+a+"; path\x3d/; domain\x3d.zenmarket.jp",console.log("utm_content cookie updated:",b)):console.log("utm_content cookie not updated or no new value provided.")})();</script>
+
+<script type="text/javascript" id="" charset="">(function(){function e(c){c+="\x3d";var b=decodeURIComponent(document.cookie);b=b.split(";");for(var f=0;f<b.length;f++){var g=b[f].trim();if(g.indexOf(c)===0)return g.substring(c.length,g.length)}return null}function k(){var c=e("gclid"),b=e("fbclid");return c?"google ads":b?"meta":null}var h="ads_source",a=e(h);console.log("Current ads_source cookie value:",a);console.log("Checking for gclid or fbclid cookies...");var d=k();console.log("Detected ad source:",d);d&&d!==a?(console.log("Updating ads_source cookie..."),
+a=new Date,a.setTime(a.getTime()+6048E5),a="; expires\x3d"+a.toUTCString(),document.cookie=h+"\x3d"+d+a+"; path\x3d/; domain\x3d.zenmarket.jp",console.log("ads_source cookie updated:",d)):console.log("ads_source cookie not updated or no new value provided.")})();</script>
+
+<script type="text/javascript" id="" charset="">if(!wcs_add)var wcs_add={};wcs_add.wa="s_4ba49f38a88e";if(!_nasa)var _nasa={};window.wcs&&(wcs.inflow(),wcs_do(_nasa));</script>
+
+
+<script id="" text="" charset="" type="text/javascript" src="//wcs.naver.net/wcslog.js"></script>
+
+<script type="text/javascript" id="" charset="">var _nasa={};window.wcs&&(_nasa.cnv=wcs.cnv("5","PageView"));</script>
